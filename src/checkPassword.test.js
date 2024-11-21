538d8ed64f8f9e1b661eb1726e82a758c2bd9950
Yarolav <yaroslav@talentpath.co>
2024-11-21 19:28:57 +0200

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -1,19 +1,45 @@
-'use strict';
+"use strict";
 
 describe(`Function 'checkPassword':`, () => {
-  const checkPassword = require('./checkPassword');
+  const checkPassword = require("./checkPassword");
 
   it(`should be declared`, () => {
     expect(checkPassword).toBeInstanceOf(Function);
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword("12");
 
+    expect(typeof result).toBe("boolean");
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const result = checkPassword("yu!12323ASA");
 
+    // write more tests here
+    expect(result).toBe(true);
   });
 
-  // write more tests here
+  // eslint-disable-next-line
+  it(`should return 'false' for the password with less than 8 characters`, () => {
+    const result = checkPassword("!3AA");
+
+    // write more tests here
+    expect(result).toBe(false);
+  });
+
+  // eslint-disable-next-line
+  it(`should return 'false' for the password with more than 16 characters`, () => {
+    const result = checkPassword("!3AA234reodjqwdqwkdqwdoqkwpdoqwjdqw");
+
+    // write more tests here
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for the not Latin alphabet`, () => {
+    const result = checkPassword("привіт!345678");
+
+    // write more tests here
+    expect(result).toBe(false);
+  });
 });
